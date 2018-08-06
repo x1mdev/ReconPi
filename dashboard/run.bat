a432b559bb3352d839a91d2f92999e61ea6eb5ce
Martijn <martijn@x1m.nl>
2018-08-06 23:15:11 +0200

    arm troubles

--- dashboard/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-recon-pi-db -importPath github.com/x1mdev/recon-pi-db -srcPath %CD%\src -runMode dev
--- dashboard/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
-"$SCRIPTPATH/recon-pi-db" -importPath github.com/x1mdev/recon-pi-db -srcPath "$SCRIPTPATH/src" -runMode dev
