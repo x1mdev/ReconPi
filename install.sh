39f45fd7c945d3a43675b4ed26bf1d1fa8c8c1d4
x1mdev <martijn@x1m.nl>
2019-09-23 02:09:38 +0200

    nmap install temporary disabled

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ basicRequirements() {
     export LANGUAGE=en_US.UTF-8
     export LANG=en_US.UTF-8
     export LC_ALL=en_US.UTF-8
-    locale-gen en_US.UTF-8
+    # locale-gen en_US.UTF-8 # not working
     sudo apt-get update -y
     # sudo apt-get upgrade -y
     sudo apt-get install -y --reinstall build-essential
@@ -231,38 +231,41 @@ additionalTools() {
         sudo python3 setup.py install
         echo -e "[$GREEN+$RESET] Done."
     fi
+    
+    # might not use this, takes A LOT of time.
 
-    echo -e "[$GREEN+$RESET] Installing nmap.."
-    # used and edited the nmap install script from capt-meelo
-    cd "$HOME"/tools/ || return
-    NMAP_VERSION="$(wget -qO- https://nmap.org/dist/ | grep -oP 'nmap-([0-9\.]+)\.tar\.bz2' | tail -n 1 | grep -oP 'nmap-[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)"
-    if [ ! -x "$(command -v nmap)" ]; then
-        echo -e "[$GREEN+$RESET] Starting installation.."
-        wget https://nmap.org/dist/nmap-"$NMAP_VERSION".tar.bz2
-        bzip2 -cd nmap-"$NMAP_VERSION".tar.bz2 | tar xvf -
-        cd "$HOME"/tools/nmap-"$NMAP_VERSION" || return
-        ./configure
-        make -j
-        sudo make -j install
-        cd "$HOME"/tools/ || return
-        rm -rf nmap-"$NMAP_VERSION"*
-    else
-        if [ "$NMAP_VERSION" == "$(nmap -V | grep version | cut -d " " -f 3)" ]; then
-            echo -e "[$GREEN+$RESET] Latest version detected..${RESET}"
-        else
-            echo -e "${BLUE}[!] Upgrading to the latest version of Nmap...${RESET}"
-            cd "$HOME"/tools/ || return
-            wget https://nmap.org/dist/nmap-"$NMAP_VERSION".tar.bz2
-            bzip2 -cd "$HOME"/tools/nmap-"$NMAP_VERSION".tar.bz2 | tar xvf -
-            cd "$HOME"/tools/nmap-"$NMAP_VERSION" || return
-            ./configure
-            make -j
-            sudo make -j install
-            cd "$HOME"/tools/ || return
-            rm -rf cd "$HOME"/tools/nmap-"$NMAP_VERSION"*
-        fi
-    fi
-    echo -e "[$GREEN+$RESET] Done."
+
+    # echo -e "[$GREEN+$RESET] Installing nmap.."
+    # # used and edited the nmap install script from capt-meelo
+    # cd "$HOME"/tools/ || return
+    # NMAP_VERSION="$(wget -qO- https://nmap.org/dist/ | grep -oP 'nmap-([0-9\.]+)\.tar\.bz2' | tail -n 1 | grep -oP 'nmap-[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)"
+    # if [ ! -x "$(command -v nmap)" ]; then
+    #     echo -e "[$GREEN+$RESET] Starting installation.."
+    #     wget https://nmap.org/dist/nmap-"$NMAP_VERSION".tar.bz2
+    #     bzip2 -cd nmap-"$NMAP_VERSION".tar.bz2 | tar xvf -
+    #     cd "$HOME"/tools/nmap-"$NMAP_VERSION" || return
+    #     ./configure
+    #     make -j
+    #     sudo make -j install
+    #     cd "$HOME"/tools/ || return
+    #     rm -rf nmap-"$NMAP_VERSION"*
+    # else
+    #     if [ "$NMAP_VERSION" == "$(nmap -V | grep version | cut -d " " -f 3)" ]; then
+    #         echo -e "[$GREEN+$RESET] Latest version detected..${RESET}"
+    #     else
+    #         echo -e "${BLUE}[!] Upgrading to the latest version of Nmap...${RESET}"
+    #         cd "$HOME"/tools/ || return
+    #         wget https://nmap.org/dist/nmap-"$NMAP_VERSION".tar.bz2
+    #         bzip2 -cd "$HOME"/tools/nmap-"$NMAP_VERSION".tar.bz2 | tar xvf -
+    #         cd "$HOME"/tools/nmap-"$NMAP_VERSION" || return
+    #         ./configure
+    #         make -j
+    #         sudo make -j install
+    #         cd "$HOME"/tools/ || return
+    #         rm -rf cd "$HOME"/tools/nmap-"$NMAP_VERSION"*
+    #     fi
+    # fi
+    # echo -e "[$GREEN+$RESET] Done."
 }
 
 : 'Dashboard setup'
